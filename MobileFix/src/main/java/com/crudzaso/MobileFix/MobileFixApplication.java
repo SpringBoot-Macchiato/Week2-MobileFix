c70689caeb7cf8fbf8b2ecf4863f996db5364041
JuanMendez98 <juancamilosanchezmendez@gmail.com>
2025-10-29 19:37:54 -0500

    git commit -m "chore(setup): fix dependencies and configure project #6903"

---
+++ MobileFix/src/main/java/com/crudzaso/MobileFix/MobileFixApplication.java
@@ -0,0 +1,13 @@
+package com.crudzaso.MobileFix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileFixApplication.class, args);
+	}
+
+}
---
+++ MobileFix/src/test/java/com/crudzaso/MobileFix/MobileFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudzaso.MobileFix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
